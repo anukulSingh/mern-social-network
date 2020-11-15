2b14e2d5e96f7da0ebc9d92910713c8dfcf03bc6
Anukul kumar <anukulsingh125@gmail.com>
2020-11-15 20:47:35 +0530

    user registration and validation

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
+require('./config/db')
+
 const app = express()
 
 const PORT = process.env.PORT || 5000
+
+app.use(express.json({ extended: false }))
+
+app.use('/api/users', require('./routes/api/user'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
 
 app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`))
