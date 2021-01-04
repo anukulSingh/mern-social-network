291e32981fa38053361f41be54e33a977f180adc
Anukul kumar <anukulsingh125@gmail.com>
2021-01-04 15:29:27 +0530

    basic redux boilerplate for alerts, yet to be fixed in UI

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,28 @@ import Navbar from './components/Layout/Navbar';
 import Landing from './components/Layout/Landing';
 import Register from './components/auth/Register'
 import Login from './components/auth/Login';
+import Alert from './components/Layout/Alert'
+//redux
+import { Provider } from 'react-redux'
+import store from './store'
 
 
 const App = () => 
+<Provider store={store}>
   <Router>
     <Fragment>
       <Navbar />
       <Route exact path='/' component= { Landing } />
-      <section className="container">
+      <div className="container">
+        <Alert />
         <Switch>
           <Route exact path="/register" component= {Register} />
           <Route exact path="/login" component= {Login} />
         </Switch>
-      </section>
+      </div>
     </Fragment>
   </Router>
+  </Provider>
 
 
 export default App;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,12 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types'
 
-const Register = () => {
+
+import { setAlert } from '../../actions/alert'
+
+const Register = ({ setAlert }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -11,15 +16,14 @@ const Register = () => {
     const { name, email, password, password2 } = formData
 
     const onChange = e => setFormData({...formData, [e.target.name]: e.target.value })
-    let flag;
     const onSubmit = async e => {
         e.preventDefault()
 
         if (password !== password2) {
-            console.log('Password does not match');
+            setAlert('Password does not match','danger');
         }
         else {
-             console.log(formData)
+             console.log('SUCCESS')
             // const newUser = {
             //     name,
             //     email,
@@ -85,5 +89,8 @@ const Register = () => {
         </Fragment>
     )
 }
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired,
+}
 
-export default Register
+export default connect(null, { setAlert })(Register)
